4af6bc2561a384d5a2351f244d3cc915a78ce521
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2016-04-24 21:26:44 +0200

    mailbox empty contructor

--- zmqu/zmqu.hpp
+++ zmqu/zmqu.hpp
@@ -28,6 +28,7 @@ class mailbox
 	: private boost::noncopyable
 {
 public:
+	mailbox() : _inproc{nullptr} {}
 	mailbox(std::string const & uri, std::shared_ptr<zmq::context_t> ctx);
 	~mailbox();
 	void send(char const * command, std::string const & message);  // TODO: not a general (implement varargs template there)
