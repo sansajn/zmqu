374ee04482d55f0e99e77d09d0e393de3ec8ec87
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2018-12-01 22:05:16 +0100

    jtree helpers

--- zmqu/json.cpp
+++ zmqu/json.cpp
@@ -1,7 +1,9 @@
-#include "json.hpp"
 #include <sstream>
 #include <boost/property_tree/json_parser.hpp>
+#include "json.hpp"
 
+using std::string;
+using std::vector;
 using std::stringstream;
 namespace pt = boost::property_tree;
 
@@ -16,8 +18,32 @@ string to_string(jtree const & json)
 
 }  // std
 
-void to_json(std::string const & s, jtree & result)
+void to_json(string const & s, jtree & result)
 {
 	stringstream ss{s};
 	pt::read_json(ss, result);
+}
+
+void vector_put(vector<string> const & v, string const & key, jtree & result)
+{
+	jtree arr;
+	for (string const & elem : v)
+	{
+		jtree local;
+		local.put("", elem);
+		arr.push_back(make_pair("", local));
+	}
+	result.add_child(key, arr);
+}
+
+void vector_put(vector<size_t> const & v, string const & key, jtree & result)
+{
+	jtree arr;
+	for (size_t const & elem : v)
+	{
+		jtree local;
+		local.put<size_t>("", elem);
+		arr.push_back(make_pair("", local));
+	}
+	result.add_child(key, arr);
 }
--- zmqu/json.hpp
+++ zmqu/json.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 #include <boost/property_tree/ptree.hpp>
 
 using jtree = boost::property_tree::ptree;
@@ -13,3 +14,7 @@ string to_string(jtree const & json);
 
 //! converts json string to tree
 void to_json(std::string const & s, jtree & result);
+
+//! put vector to jtree
+void vector_put(std::vector<std::string> const & v, std::string const & key, jtree & result);
+void vector_put(std::vector<size_t> const & v, std::string const & key, jtree & result);
